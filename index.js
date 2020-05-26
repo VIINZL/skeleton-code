8e2e6fa936698d6d0e6246fad822731e228e3c8f
viinz <42003446+VIINZL@users.noreply.github.com>
2020-05-26 19:24:04 +0200

    added shard id's as .env var

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { totalShards: 23 });
+let shards = process.env.SHARDS;
+let shards = parseInt(shards, 10);
+const manager = new ShardingManager('./bot.js', { totalShards: shards });
 
 manager.spawn();
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
