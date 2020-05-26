065f2612eea4bb5578b93d9c64c0c05b90d7e196
VIINZL <v.heidenreich@icloud.com>
2020-05-26 15:46:56 +0200

    whoops, stupid

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', options.totalShards = '23');
-
+const manager = new ShardingManager('./bot.js');
+manager.options.totalShards = '23';
 manager.spawn();
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
