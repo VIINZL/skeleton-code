34abeb63f389ccc49d37b5b96ea5da65a53c59bf
VIINZL <v.heidenreich@icloud.com>
2020-05-26 15:45:06 +0200

    updated shard count

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-require('dotenv').config();
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN });
+const manager = new ShardingManager('./bot.js', options.totalShards = '23');
 
 manager.spawn();
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
