0e6bde08fe2106a79562ce66e0a07e7c1a751529
viinz <42003446+VIINZL@users.noreply.github.com>
2020-05-26 20:05:09 +0200

    next try for shard spawn log

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const rawshards = process.env.SHARDS;
 const shards = parseInt(rawshards, 10);
 const manager = new ShardingManager('./bot.js', { totalShards: shards });
 
+function checkshards(shard, shards) => {
+  if (shards - shard.id === 0) {
+    console.log('Ready! (all shards have been spawned)');
+  };
+};
+
 manager.spawn();
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-manager.on('shardCreate', shard => if (shards - shard === 0) console.log('Ready! (all shards are spawned)'));
+manager.on('shardCreate', shard => checkshards(shard, shards));
