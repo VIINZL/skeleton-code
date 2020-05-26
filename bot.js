40a9d48956369c3aedddb0b1923413986611744e
VIINZL <v.heidenreich@icloud.com>
2020-05-26 15:21:17 +0200

    .env and .gitignore added copied default skeleton code

---
+++ bot.js
@@ -0,0 +1,16 @@
+const { Client } = require('discord.js');
+const client = new Client();
+const prefix = '!';
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'stats') {
+		return message.channel.send(`Server count: ${client.guilds.cache.size}`);
+	}
+});
+
+client.login(process.env.TOKEN);
