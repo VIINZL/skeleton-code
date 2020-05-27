a549337b3e3f14f427ebc0ea413075be9f96d019
viinz <42003446+VIINZL@users.noreply.github.com>
2020-05-27 15:29:47 +0200

    dotenv require missed and manager options fixed

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const { ShardingManager } = require('discord.js');
 const rawshards = process.env.SHARDS;
 const shards = parseInt(rawshards, 10);
-const manager = new ShardingManager('./bot.js', { totalShards: shards } { shardList: [0, 1]});
+const manager = new ShardingManager('./bot.js', { totalShards: shards, shardList: [0, 1]});
 
 function checkShards(shard, shards) {
   if (shards - shard.id === 0) {
