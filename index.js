fed8924ea1a17cd368deaeec897956994c4b5d9d
viinz <42003446+VIINZL@users.noreply.github.com>
2020-05-27 15:02:48 +0200

    added shard list

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { ShardingManager } = require('discord.js');
 const rawshards = process.env.SHARDS;
 const shards = parseInt(rawshards, 10);
-const manager = new ShardingManager('./bot.js', { totalShards: shards });
+const manager = new ShardingManager('./bot.js', { totalShards: shards } { shardList: [0, 1]});
 
 function checkShards(shard, shards) {
   if (shards - shard.id === 0) {
